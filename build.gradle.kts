28d5af7b8b1a08a1586fdce81e6ee38e15f8cdd9
J03-M4NT <jmantillah1503@gmail.com>
2025-11-14 20:30:38 -0500

    feat: Integrate Firebase Authentication
    Adds Firebase to the project to handle user authentication.
    
    - Includes the `google-services.json` configuration file for the `tata-firebase25` project.
    - Adds Firebase Authentication and Firestore dependencies (`firebase-bom`, `firebase-auth`, `firebase-firestore`).
    - Configures the Gradle build files with the `google-services` plugin.
    - Implements `FirebaseAuthManager.kt` with functions for user login (`loginuser`) and logout.
    - Adds the `INTERNET` permission to `AndroidManifest.xml`.
    - Updates App Insights and adds Studio Bot settings.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,9 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+
+    // Dependencia de Google-services gradle plugin
+    id("com.google.gms.google-services") version "4.4.4" apply false
+
 }
