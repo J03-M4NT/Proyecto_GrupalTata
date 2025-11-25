6de79e3af9ca08a1dfcaa6b395339e7a900b2c93
Jaime Castro <19100255@ue.edu.pe>
2025-11-25 14:55:14 -0500

    colaboradores del mapping dentro de las tarjetas de las vacantes creadas y/o registradas

--- app/src/main/java/com/example/proyecto_grupaltata/presentation/navigation/AppNavGraph.kt
+++ app/src/main/java/com/example/proyecto_grupaltata/presentation/navigation/AppNavGraph.kt
@@ -2,14 +2,15 @@ package com.example.proyecto_grupaltata.presentation.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.rememberNavController
-import com.example.proyecto_grupaltata.presentation.matching.MatchingScreen
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.proyecto_grupaltata.presentation.auth.LoginScreen
 import com.example.proyecto_grupaltata.presentation.brechas.BrechasScreen
 import com.example.proyecto_grupaltata.presentation.home.HomeScreen
+import com.example.proyecto_grupaltata.presentation.mapping.MappingViewModel
+import com.example.proyecto_grupaltata.presentation.matching.MatchingScreen
 import com.example.proyecto_grupaltata.presentation.more.MoreScreen
 import com.example.proyecto_grupaltata.presentation.report_activity.ReportActivityScreen
 import com.example.proyecto_grupaltata.presentation.vacancies.VacanciesScreen
@@ -17,26 +18,34 @@ import com.example.proyecto_grupaltata.presentation.vacancies.VacanciesScreen
 @Composable
 fun AppNavGraph(navController: NavHostController, modifier: Modifier) {
 
+    // Create a single instance of the ViewModel to be shared
+    val mappingViewModel: MappingViewModel = viewModel()
 
     NavHost(
         navController = navController,
-        startDestination = AppScreens.LoginScreen.route){    // La primera ruta donde comienza la app
+        modifier = modifier,
+        startDestination = AppScreens.LoginScreen.route
+    ) {
 
         composable(AppScreens.LoginScreen.route) { LoginScreen(navController) }
-        
+
         composable(AppScreens.HomeScreen.route) { HomeScreen(navController) }
-        
-        composable(AppScreens.VacanciesScreen.route) { VacanciesScreen(navController) }
+
+        composable(AppScreens.VacanciesScreen.route) {
+            // Pass the shared ViewModel to VacanciesScreen
+            VacanciesScreen(navController = navController, mappingViewModel = mappingViewModel)
+        }
 
         composable(AppScreens.MoreScreen.route) { MoreScreen(navController) }
 
-        composable(AppScreens.MatchingScreen.route) { MatchingScreen(navController) }
+        composable(AppScreens.MatchingScreen.route) {
+            // Pass the shared ViewModel to MatchingScreen (Mapping)
+            MatchingScreen(navController = navController, mappingViewModel = mappingViewModel)
+        }
 
         composable(AppScreens.BrechasScreen.route) { BrechasScreen() }
 
         composable(AppScreens.ReportActivityScreen.route) { ReportActivityScreen(navController) }
- 
-        // Demas pantallas
-        
+
     }
 }
